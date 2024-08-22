d92de726509761ea0802ba4958d9b7c890c6c829
Luke <nycthbre@gmail.com>
2024-08-22 12:54:05 +0200

    Exercise 6.20 completion (query-anecdotes)

---
+++ query-anecdotes/src/components/ErrorPage.jsx
@@ -0,0 +1,9 @@
+const ErrorPage = () => {
+    return(
+        <div>
+            <h2>Anecdote service not available due to problems in server</h2>
+        </div>
+    )
+}
+
+export default ErrorPage
